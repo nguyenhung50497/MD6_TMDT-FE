4dc3430558587a88bda846aa858038189c8ce6a8
ChatNghe <namnt98@gmail.com>
2023-03-15 15:44:55 +0700

    commit lai

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -86,10 +86,7 @@ export default function Navbar() {
                 setQueryStringAPI(queryString)
                 navigate('/search?' + queryString,{state: queryString})
             }
-            // if (queryString && existUrl !== '') {
-            //     setQueryStringAPI(existUrl)
-            //     navigate('/home/search?' + existUrl,{state: queryString})
-            // }
+
             if (!queryString && existUrl !== '')  {
                 setQueryStringAPI(queryString)
                 navigate('/search')
@@ -98,6 +95,8 @@ export default function Navbar() {
             //     setQueryStringAPI(queryString)
             //     navigate('/')
             // }
+
+
         }, [queryValue.keyword[0]])
         useEffect(() => {
             dispatch(search(queryStringAPI));
@@ -269,48 +268,3 @@ export default function Navbar() {
     )
 }
 
-
-    // return (
-    //     <>
-    //         <div className="row">
-    //             <div className="col-12">
-    //                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    //                     <Link className="navbar-brand" to="/home">Logo</Link>
-    //                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-    //                             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    //                         <span className="navbar-toggler-icon"></span>
-    //                     </button>
-    //                     <div className="collapse navbar-collapse" id="navbarNav">
-    //                         <ul className="navbar-nav">
-    //                             <li className="nav-item active">
-    //                                 <Link className="nav-link" to="add-blog">Add Blog <span
-    //                                     className="sr-only">(current)</span></Link>
-    //                             </li>
-    //                             <li className="nav-item">
-    //                                 <a className="nav-link" href="#">Features</a>
-    //                             </li>
-    //                             <li className="nav-item">
-    //                                 <a className="nav-link" href="#">Pricing</a>
-    //                             </li>
-    //                             <li className="nav-item">
-    //                                 <a className="nav-link disabled">Disabled</a>
-    //                             </li>
-    //                         </ul>
-    //
-    //                         <Formik initialValues={{keyword: ['']}} onSubmit={handleSubmit}>
-    //                             <Form>
-    //                                 <div className="form-group">
-    //                                     <Field type="text" name="keyword"/>
-    //                                     <button type="submit">Apply</button>
-    //                                 </div>
-    //
-    //                             </Form>
-    //                         </Formik>
-    //
-    //
-    //                     </div>
-    //                 </nav>
-    //             </div>
-    //         </div>
-    //     </>
-    // )
