cc9fab6cae68a96f6a135f9975a1f64306c65d4d
ChatNghe <namnt98@gmail.com>
2023-03-23 13:59:23 +0700

    sua lai thong ke

--- src/service/statsService.js
+++ src/service/statsService.js
@@ -1,7 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import customAxios from "./api";
 export const sales = createAsyncThunk("sales/sales", async (search) => {
-   const res = await customAxios.get("cart-details/stats/sales?" + search);
-   console.log(res.data);
+   const res = await customAxios.get("cart-details/stats?" + search);
    return res.data;
 });
